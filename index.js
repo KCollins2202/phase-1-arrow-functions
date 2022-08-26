05a9160b8aa095f1111a7cb542e56e8627a555d2
Collins Kimani Karanja <kimcolinss@gmail.com>
2022-08-26 22:18:07 +0300

    Completed asignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function divide(num1=2000,num2=100 ) {
+    return num1/num2;
+
+    }
+    
+    const square = x => x**2;
+    
+    const add = (para1=1,para2=2) => { 
+        return para1+para2 }
